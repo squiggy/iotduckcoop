df15b59890167d272514d473eeeda06bdf94a74e
Squiggy Rubio <squiggy@users.noreply.github.com>
2026-04-16 12:10:48 -0600

    Save work

--- src/iotduckcoop.cpp
+++ src/iotduckcoop.cpp
@@ -12,8 +12,16 @@
 // Let Device OS manage the connection to the Particle Cloud
 SYSTEM_MODE(AUTOMATIC);
 
-const int MOTIONSENSOR = D7;
+////// ------------------------- Shared variables ------------------------- //////
 
+const int MOTIONSENSOR = D7;
+int motionSensor;
+
+// const int PHOTODIODE_DAYLIGHT = A3;
+const int LED_DAYLIGHT_TEST = A4;
+int brightness;
+
+int t;
 
 ////// ------------------------- Door 1 / Outer Door ------------------------- //////
 
@@ -26,9 +34,10 @@ const int STEP_DOOR1 = D4;
 const int LASERDIODE_DOOR1 = A0;
 
 const int PHOTODIODE_DOOR1 = A2;
+int photodiodeDoor1;
 
-const int SWITCH_STOP_OPENING_DOOR1 = D15;
-const int SWITCH_STOP_CLOSEING_DOOR1 = D8;
+const int SWITCH_STOP_OPENING_DOOR1 = D17;
+const int SWITCH_STOP_CLOSEING_DOOR1 = D18;
 bool switchStopOpeningDoor1;
 
 bool isOuterDoorOpen();
@@ -45,10 +54,11 @@ const int STEP_DOOR2 = D6;
 const int LASERDIODE_DOOR2 = A1;
 
 const int PHOTODIODE_DOOR2 = A5;
+int photodiodeDoor2;
 
 // TODO - wire swiches for Door 2
-const int SWITCH_STOP_OPENING_DOOR2 = D18;
-const int SWITCH_STOP_CLOSEING_DOOR2 = D17;
+const int SWITCH_STOP_OPENING_DOOR2 = D15;
+const int SWITCH_STOP_CLOSEING_DOOR2 = D8;
 bool switchStopOpeningDoor2;
 
 bool isInnerDoorOpen();
@@ -64,11 +74,6 @@ const int steps = 200 * microMode;
 
 int checkDawnTimer; // 5 minutes
 
-////// ------------------------- Variables ------------------------- //////
-
-int motionSensor;
-int photodiodeDoor1;
-int photodiodeDoor2;
 
 void setup() {
 
@@ -77,6 +82,7 @@ void setup() {
   waitFor( Serial.isConnected,10000); // wait for Serial monitor
 
   pinMode(MOTIONSENSOR, INPUT);
+  pinMode(LED_DAYLIGHT_TEST, OUTPUT);
 
   pinMode(HALLEFFECT_DOOR1, INPUT);
   pinMode(HALLEFFECT_DOOR2, INPUT);
@@ -118,7 +124,7 @@ void setup() {
 }
 
 void loop() {
- ////// ------------------------- Test Motors ------------------------- //////
+ ////// ------------------------- Tests ------------------------- //////
 
   // // change direction every loop
   // digitalWrite(DIR_DOOR1, !digitalRead(DIR_DOOR1));
@@ -144,54 +150,75 @@ void loop() {
   // }
   // delay(1000); // 1 second delay
 
+
+
   analogWrite(LASERDIODE_DOOR1, 128);  // Laser ON
   analogWrite(LASERDIODE_DOOR2, 128);  // Laser ON
 
 ////// ------------------------- Motion Sensor ------------------------- //////
 
   motionSensor = digitalRead(MOTIONSENSOR);
-  // Serial.printf("motionSensor %i \n", motionSensor);
+  Serial.printf("motionSensor %i \n", motionSensor);
   // If there is motion in chamber, close outer door and open inner door to 
   // let in duck.
-  if (motionSensor > 30) {
-    // Close outer door
-   // closeOuterDoor(); // DONE
-    // Open inner door, to let ducks inside.
+
+ // If it's dark, let ducks inside coop if motion is detected 
+  if (motionSensor && photodiodeDaylight < 15) {
+    // close outer door
+    closeOuterDoor(); // DONE
+    Serial.printf("Line 169: closeOuterDoor()! \n");
+    // open inner door
     openInnerDoor();
-    Serial.printf("Line 162: openInnerDoor()! \n");
+    Serial.printf("Line 172: openInnerDoor()! \n");
+  }
+
+  if (motionSensor == 0) {
+    openOuterDoor(); // DONE
+    closeInnerDoor();
   }
 
 ////// ------------------------- Daylight ------------------------- //////
 
-  // Check photodiode for daylight
-  // photodiodeDaylight = analogRead(PHOTODIODE_DAYLIGHT);
-  // Serial.printf("photodiodeDaylight %i \n", photodiodeDaylight);
+  // Use LED to simulate daylight (for testing)
+  t = millis() / 5000.0;
+  brightness = 127.5 * sin(2*M_PI*(1/5.0)*t)+127.5;
+  analogWrite(LED_DAYLIGHT_TEST, brightness);
 
-      // if((millis() - checkDawnTimer) > 500) {
-        // if (isDawn() && !isOuterDoorOpen() && !isInnerDoorOpen()) {
-        if (isDawn()) {
-          // Serial.printf("Line 174: checkDawnTimer %i \n", checkDawnTimer);
-          openOuterDoor();
-          // openInnerDoor(); // Done
-          // TODO - determine way to keep track of time for dawn and dusk, as this time changes through the year.
-        }
-        // checkDawnTimer = millis();
-      // }
+  // Use photodiode to detect daylight.
+  photodiodeDaylight = analogRead(PHOTODIODE_DAYLIGHT);
+  Serial.printf("photodiodeDaylight %i \n", photodiodeDaylight);
 
-////// ------------------------- Outer door ------------------------- //////
+  // Open coors if  there is daylight
+  if((millis() - checkDawnTimer) > 1000) {
+  //   if (isDawn() && !isOuterDoorOpen() && !isInnerDoorOpen()) {
+    if (photodiodeDaylight > 30) {
+      Serial.printf("Line 189: checkDawnTimer %i \n", checkDawnTimer);
+      openOuterDoor();
+      openInnerDoor(); // Done
+    // TODO - determine way to keep track of time for dawn and dusk, as this time changes through the year.
+    }
+    checkDawnTimer = millis();
+
+    // Test starting point
+    delay(3000);
+    Serial.printf("Line 198: cose door so we can test again :)\n ");
+
+  }
+
+////// ------------------------- Outer door safety ------------------------- //////
 
   photodiodeDoor1 = analogRead(PHOTODIODE_DOOR1);
   if (photodiodeDoor1 > 50) {
     // Open door so duck doesn't get squished!
-    // digitalWrite(STEP_DOOR1, HIGH);
+    openOuterDoor();
   }
   // Serial.printf("photodiodeDoor1 %i \n", photodiodeDoor1);
 
-////// ------------------------- Inner door ------------------------- //////
+////// ------------------------- Inner door Safety ------------------------- //////
 
   photodiodeDoor2 = analogRead(PHOTODIODE_DOOR2);
   if (photodiodeDoor2 > 50) {
-    // Open door so duck doesn't get squished!
+    openInnerDoor();
   }
   // Serial.printf(" %i \n", photodiodeDoor2);
 }
@@ -250,7 +277,18 @@ void closeOuterDoor() {
 
 // TODO - finish getting this working
 void closeInnerDoor() {
+  int switchStopClosingDoor2 = digitalRead(SWITCH_STOP_CLOSEING_DOOR2);
+  Serial.printf("switchStopClosingDoor2 %d \n", switchStopClosingDoor2);
 
+  digitalWrite(DIR_DOOR2, LOW);
+  if (!switchStopClosingDoor2) {
+    for(int x = 0; x < steps; x++) {
+      digitalWrite(STEP_DOOR2, HIGH);
+      delay(2);
+      digitalWrite(STEP_DOOR2, LOW);
+      delay(2);
+    }
+  }
 };
 
 bool isOuterDoorOpen() {
